70d3af7ad3d85ea239d0809071e732808bd3c94e
Byron Blocker and Emily Dokken <emily@>
2017-02-22 21:02:26 -0800

    redo logic behind triangle identifier using sides rather than angles and branching

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,28 @@ $(document).ready(function(){
       var angle2 = parseInt($("input#angle2").val());
       var angle3 = parseInt($("input#angle3").val());
 
-      if (angle1 + angle2 + angle3 !=180) {
-         $(".answer").text("not a triangle");
-      }
+      // console.log(angle1);
+      // console.log(angle2);
+      // console.log(angle3);
 
-      if (angle1 === angle2 && angle2 === angle3 && angle3 === angle1) {
+      if (angle1 + angle2 <= angle3 || angle3 + angle2 <= angle1 || angle3 + angle1<= angle2) {
+         $(".answer").text("not a triangle");
+      } else if (angle1 != angle2 && angle1 != angle3 && angle2 != angle3) {
+         $(".answer").text("is a Scalene triangle!");
+       }
+
+
+      else if (angle1 === angle2 && angle2 === angle3 && angle3 === angle1) {
         $(".answer").text("Is an equilateral triangle");
       }
 
-      if (angle1 === angle2 || angle1 === angle3 || angle2 === angle3) {
+      else if (angle1 === angle2 != angle3 || angle1 === angle3 != angle2 || angle2 === angle3 != angle1) {
         $(".answer").text("is an Isosceles triangle");
       }
 
-      if (angle1 != angle2 && angle1 != angle3 && angle2 != angle3) {
-        $(".answer").text("is a Scalene triangle!");
-      }
+      // if (angle1 != angle2 && angle1 != angle3 && angle2 != angle3) {
+      //   $(".answer").text("is a Scalene triangle!");
+      // }
 
     event.preventDefault();
     });
